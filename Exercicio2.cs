1d9a0105bd8c0fe0fe4595208f149135a19385ee
Mattoskkj <joaomattos2309@gmail.com>
2026-02-19 11:33:46 -0300

    Entregando Ex1, 2 e 3

---
+++ Exercicio2.cs
@@ -0,0 +1,12 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+    //private static string nome = "Joao Mattos";
+    
+    public static void Executar()
+    {
+        string nome = "Joao Mattos";
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
---
+++ Exercicio4.cs
@@ -0,0 +1,14 @@
+// -- Arquivo Exercicio1.cs
+public static class Exercicio4
+{
+    public static void Executar()
+    {
+		   string nome = "Joao Mattos";
+           string idade = "16 anos";
+           string passatempo = "Volei";
+
+           Console.WriteLine(nome);
+           Console.WriteLine(idade);
+           Console.WriteLine(passatempo);
+}
+}
